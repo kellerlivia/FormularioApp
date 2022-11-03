44fc96fcf62ccec7055da2580a9ceb739ea9e761
kellerlivia <rm94275@fiap.com.br>
2022-11-03 15:30:45 -0300

    Validando o email

--- FormularioApp/Model/ValidateForm.swift
+++ FormularioApp/Model/ValidateForm.swift
@@ -43,7 +43,15 @@ class ValidateForm: NSObject {
         }
         
         guard let cpf = dictionaryOfTextFields[.cpf], BooleanValidator().validate(cpf: cpf.text!) else { return false }
+        guard let email = dictionaryOfTextFields[.email], self.checkEmail(email.text!) else { return false }
         return true
+    }
+    
+    func checkEmail(_ email: String) -> Bool {
+        let emailRegEx = "(?:[a-z0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&'*+/=?\\^_`{|}"+"~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\"+"x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-"+"z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5"+"]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-"+"9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21"+"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+        
+        let emailTest = NSPredicate(format: "SELF MATCHES[c] %@", emailRegEx)
+        return emailTest.evaluate(with: email)
     }
     
     func displaysFilledTextFieldsNotification(title: String, message: String) -> UIAlertController {
